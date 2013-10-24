499a6536792fcdb94a2218b4a2ea9d0ef5878246
xcargar <carlosags@fitec.org.br>
2013-10-24 20:24:06 +0100

    implementing console task and removing customPlugin stuff

---
+++ src/main/groovy/org/gradle/ConsolePlugin.groovy
@@ -0,0 +1,14 @@
+package org.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.tasks.JavaExec
+
+class ConsolePlugin implements Plugin<Project> {
+	void apply(Project project) {
+		project.rootProject.task('console', dependsOn: 'classes', type: JavaExec) {
+			main = 'groovy.ui.Console'
+			classpath = project.sourceSets.main.runtimeClasspath
+		}
+	}
+}
