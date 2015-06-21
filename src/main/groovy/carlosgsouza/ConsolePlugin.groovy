cd519faaba87456fdc9874788eb9659745759795
carlosgsouza <carlosgsouza@gmail.com>
2015-06-20 23:09:23 -0700

    Adding test dependencies to classpath

--- src/main/groovy/carlosgsouza/ConsolePlugin.groovy
+++ src/main/groovy/carlosgsouza/ConsolePlugin.groovy
@@ -4,10 +4,16 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.tasks.JavaExec
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 class ConsolePlugin implements Plugin<Project> {
+	
 	void apply(Project project) {
 		project.rootProject.task('console', dependsOn: 'classes', type: JavaExec) {
+			logger.debug("Opening Gradle Console")
+			
 			main = 'groovy.ui.Console'
 			
 			project.apply plugin: "groovy"
@@ -15,7 +21,8 @@ class ConsolePlugin implements Plugin<Project> {
 			Configuration consoleRuntime = project.configurations.create("consoleRuntime")
 			consoleRuntime.dependencies.add(project.dependencies.localGroovy())
 			
-			classpath = project.sourceSets.main.runtimeClasspath + project.files(consoleRuntime.asPath)
+			classpath = project.sourceSets.main.runtimeClasspath + project.sourceSets.test.runtimeClasspath  + project.files(consoleRuntime.asPath)
+			logger.debug("Gradle Console classpath=$classpath")
 		}
 	}
 }
--- src/test/groovy/carlosgsouza/ConsolePluginSpec.groovy
+++ src/test/groovy/carlosgsouza/ConsolePluginSpec.groovy
@@ -1,7 +1,8 @@
 package carlosgsouza
 
 import org.gradle.api.Project
-import org.gradle.api.artifacts.DependencySet
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.internal.artifacts.dependencies.DefaultSelfResolvingDependency
 import org.gradle.testfixtures.ProjectBuilder
 
 import spock.lang.Specification
@@ -35,5 +36,32 @@ class ConsolePluginSpec extends Specification {
 		
 		and: "the console task classpath includes dependencies from consoleRuntime configuration"
 		project.tasks.console.classpath.find{ it ==~ /.*groovy-all.*.jar/ }
+	}
+	
+	def "should include dependencies of all configurations in the console classpath"() {
+		given:
+		Project project = ProjectBuilder.builder().build()
+		project.apply plugin: "groovy"
+		
+		and:
+		addDependency(project, "compile", "lib/compile.jar")
+		addDependency(project, "runtime", "lib/runtime.jar")
+		addDependency(project, "testCompile", "lib/testCompile.jar")
+		addDependency(project, "testRuntime", "lib/testRuntime.jar")
+		
+		when:
+		project.apply plugin: ConsolePlugin
+
+		then:
+		project.tasks.console.classpath.find{ it ==~ /.*compile.jar.*/ }
+		project.tasks.console.classpath.find{ it ==~ /.*runtime.jar.*/ }
+		project.tasks.console.classpath.find{ it ==~ /.*testCompile.jar.*/ }
+		project.tasks.console.classpath.find{ it ==~ /.*testRuntime.jar.*/ }
+		
+	}
+	
+	private void addDependency(Project project, String configuration, String dependency) {
+		FileCollection dependencyAbsolutePath = project.files(new File(dependency).absolutePath)
+		project.configurations[configuration].dependencies.add(new DefaultSelfResolvingDependency(dependencyAbsolutePath))
 	}
 }
